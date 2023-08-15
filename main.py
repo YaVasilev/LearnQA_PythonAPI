8fb150d0b48c56416c9514e29e05f1be7888d659
Yaroslav Vasilev <ia.vasilev@petrovich.tech>
2023-08-15 18:16:01 +0300

    Ex4: GET-запрос

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import requests
 
 print("Hello from Yaroslav")
 
-response = requests.get('https://playground.learnqa.ru/api/hello', verify=False)
+response = requests.get('https://playground.learnqa.ru/api/get_text', verify=False)
 print(response.text)
